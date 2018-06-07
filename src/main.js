85dc11966b543d39eb651811f153337bcee4be4e
Dominik Kundel <dominik.kundel@gmail.com>
2018-06-07 18:09:42 +0200

    :sparkles: Add very basic component

--- src/main.js
+++ src/main.js
@@ -1,5 +1,33 @@
-console.log('Hello');
+import { html, LitElement } from '@polymer/lit-element';
+import { repeat } from 'lit-html/lib/repeat';
 
-export default function hello() {
-  console.log('something');
+export default class EmojiRating extends LitElement {
+  static get properties() {
+    return { value: Number, min: Number, max: Number, emoji: String };
+  }
+
+  constructor() {
+    super();
+    this.min = 0;
+    this.max = 5;
+    this.value = 0;
+    this.emoji = '🐼';
+  }
+
+  _render({ min, max, value, emoji }) {
+    const emojiArray = [...emoji.repeat(max)];
+    return html`
+    <style>
+      .rating { display: flex; font-size: 3em; } .emoji { color: rgba(0, 0, 0, 0); text-shadow: 0px 0px 0px var(--emoji-rating-unselected-color,
+      #444); } .emoji.active { color: rgba(0, 0, 0, 1); }
+    </style>
+    <div class="rating" aria-role="range" aria-valuemin$="${min}" aria-valuemax$="${max}" aria-valuenow$="${value}">
+      ${repeat(emojiArray, (emoji, idx) => idx, (emoji, idx) => {
+        return html`<div class$="emoji ${idx < value ? 'active' : ''}">${emoji}</div>`
+      })}
+    </div>
+    `;
+  }
 }
+
+customElements.define('emoji-rating', EmojiRating);
