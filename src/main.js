eb98b71073f91b6bd89f9b62fd4b947fc89c7cc5
Dominik Kundel <dominik.kundel@gmail.com>
2018-06-07 18:50:40 +0200

    :sparkles: Add click to change value

--- src/main.js
+++ src/main.js
@@ -12,21 +12,43 @@ export default class EmojiRating extends LitElement {
     this.max = 5;
     this.value = 0;
     this.emoji = '🐼';
+    this._renderEmoji = this._renderEmoji.bind(this);
+    this._setValueOnClick = this._setValueOnClick.bind(this);
   }
 
   _render({ min, max, value, emoji }) {
     const emojiArray = [...emoji.repeat(max)];
     return html`
     <style>
-      .rating { display: flex; font-size: 3em; } .emoji { color: rgba(0, 0, 0, 0); text-shadow: 0px 0px 0px var(--emoji-rating-unselected-color,
-      #444); } .emoji.active { color: rgba(0, 0, 0, 1); }
+      .rating {
+        display: flex;
+        font-size: 3em;
+      }
+      .emoji {
+        cursor: pointer;
+        color: rgba(0, 0, 0, 0);
+        text-shadow: 0px 0px 0px var(--emoji-rating-unselected-color, #444);
+      }
+      .emoji.active { color: rgba(0, 0, 0, 1); }
     </style>
     <div class="rating" aria-role="range" aria-valuemin$="${min}" aria-valuemax$="${max}" aria-valuenow$="${value}">
-      ${repeat(emojiArray, (emoji, idx) => idx, (emoji, idx) => {
-        return html`<div class$="emoji ${idx < value ? 'active' : ''}">${emoji}</div>`
-      })}
+      ${repeat(emojiArray, (emoji, idx) => idx, this._renderEmoji)}
     </div>
     `;
+  }
+
+  async _setValueOnClick(evt) {
+    const value = parseInt(evt.target.dataset.idx, 10) + 1;
+    this.value = value;
+    await this.renderComplete;
+    return this.value;
+  }
+
+  _renderEmoji(emoji, idx) {
+    const value = this.value;
+    return html`<div class$="emoji ${
+      idx < value ? 'active' : ''
+    }" data-idx$="${idx}" on-click="${this._setValueOnClick}">${emoji}</div>`;
   }
 }
 
